c506b4b57742b0ff906ca443d22985f0c67d26a5
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2011-09-26 01:52:37 +0900

    first omnitest(twitter)"

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, 'tvBHjRuJt3P74JN3GOHqA', 'Ws6diZ3Cyu8Rmh9W0VYsBIpwzhYUMGNeoVyNWRBPk'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniauthTest::Application.config.secret_token = 'b635527a3728108a5516e295baacd8c6be972ea28550f0e88c397360ca2610091a44f24ffacd170b7e75ea60cdba02e8f375ca60bce8a8430a11edca29013360'
